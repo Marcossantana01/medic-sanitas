933f84711acb46c96baa568e089a091d76235747
Marcos Santana <marcossantanadev@gmail.com>
2026-06-13 17:41:21 -0300

    Fix mobile navbar menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 import { FaRegCalendarCheck } from "react-icons/fa";
 import logo from "@/assets/img/logo.jpeg";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const closeMenu = () => setIsOpen(false);
+
   return (
     <header className="site-navbar bg-white">
       <nav className="navbar navbar-expand-lg navbar-light container px-0">
-        <Link href="/" className="navbar-brand brand-logo">
+        <Link href="/" className="navbar-brand brand-logo" onClick={closeMenu}>
           <Image
             src={logo}
             alt="Odonto Medic Sanitas Centro Medico e Odontologico"
@@ -23,44 +30,52 @@ export default function Navbar() {
         <button
           className="navbar-toggler border-0"
           type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navMenu"
           aria-controls="navMenu"
-          aria-expanded="false"
+          aria-expanded={isOpen}
           aria-label="Toggle navigation"
+          onClick={() => setIsOpen((current) => !current)}
         >
           <span className="navbar-toggler-icon" />
         </button>
 
-        <div className="collapse navbar-collapse justify-content-center" id="navMenu">
+        <div
+          className={`collapse navbar-collapse justify-content-center ${
+            isOpen ? "show" : ""
+          }`}
+          id="navMenu"
+        >
           <ul className="navbar-nav align-items-center gap-4 nav-links">
             <li className="nav-item">
-              <Link href="/" className="nav-link px-0">
+              <Link href="/" className="nav-link px-0" onClick={closeMenu}>
                 Inicio
               </Link>
             </li>
             <li className="nav-item">
-              <Link href="#sobre" className="nav-link px-0">
+              <Link href="#sobre" className="nav-link px-0" onClick={closeMenu}>
                 Sobre nos
               </Link>
             </li>
             <li className="nav-item">
-              <Link href="#solucoes" className="nav-link px-0">
+              <Link href="#solucoes" className="nav-link px-0" onClick={closeMenu}>
                 Servicos
               </Link>
             </li>
             <li className="nav-item">
-              <Link href="#planos" className="nav-link px-0">
+              <Link href="#planos" className="nav-link px-0" onClick={closeMenu}>
                 Planos
               </Link>
             </li>
             <li className="nav-item">
-              <Link href="#contato" className="nav-link px-0">
+              <Link href="#contato" className="nav-link px-0" onClick={closeMenu}>
                 Contato
               </Link>
             </li>
             <li className="nav-item d-lg-none w-100 pt-3">
-              <Link href="#contato" className="btn btn-success btn-sm w-100">
+              <Link
+                href="#contato"
+                className="btn btn-success btn-sm w-100"
+                onClick={closeMenu}
+              >
                 <FaRegCalendarCheck className="me-2" />
                 Area do cliente
               </Link>
